2e104ff1cc2812543e3fda588a6e8d37115ea1e1
Fallog <fallog@mailo.com>
2023-01-09 15:47:43 +0100

    Erasing pass keywords

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -10,33 +10,29 @@ class Sphere(object):
 
     # *** STEP 2 ***
     # uncomment the following 2 lines
-    #def __str__(self):
-    #    return '%s(%s)' % (self.__class__.__name__, self.radius)
+    def __str__(self):
+        return '%s(%s)' % (self.__class__.__name__, self.radius)
 
     def surface(self):
         # *** STEP 3 ***
         # uncomment the following line
-        #return 4.0 * 3.1416 * self.radius ** 2
-        pass
+        return 4.0 * math.pi * self.radius ** 2
 
     def volume(self):
         # *** STEP 4 ***
         # uncomment the following line
         return 4.0/3.0 * math.pi * self.radius ** 3
-        pass
 
     def diameter(self):
         # *** STEP 5 ***
         # uncomment the following line
         return self.radius * 2
-        pass
 
     def dump(self, filename):
         # *** STEP 6 ***
         # uncomment the following 2 lines
         with open(filename, "w") as f:
             pickle.dump(self, f)
-        pass
 
 def loadSphere(filename):
     # *** STEP 7 ***
